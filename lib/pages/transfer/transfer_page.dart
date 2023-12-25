d7531df7cd38289c3abe31f06de7bcbf6a857f80
abdulazizpermana <abdulazizpermana106@gmail.com>
2023-12-25 15:00:11 +0700

    add transfer pafe & custom widget

--- lib/pages/transfer/transfer_page.dart
+++ lib/pages/transfer/transfer_page.dart
@@ -1,5 +1,8 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/widgets/buttons.dart';
 import 'package:bank_sha/widgets/custom_form.dart';
+import 'package:bank_sha/widgets/transfer_recent_user_item.dart';
+import 'package:bank_sha/widgets/transfer_result_item.dart';
 import 'package:flutter/material.dart';
 
 class TransferPage extends StatelessWidget {
@@ -31,9 +34,99 @@ class TransferPage extends StatelessWidget {
           const SizedBox(
             height: 14,
           ),
-          CustomFormField(
+          const CustomFormField(
             title: 'by username',
             isShowTitle: false,
+          ),
+          // buildRecentUsers(),
+          buildResult(),
+          const SizedBox(
+            height: 274,
+          ),
+          CustomButtonFilled(
+            title: 'Continue',
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildRecentUsers() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recent Users',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const TransferRecentUserItem(
+            imageUrl: 'assets/img_friend1.png',
+            isVerified: true,
+            name: 'Dandi Jaelani',
+            username: 'dajal087176',
+          ),
+          const TransferRecentUserItem(
+            imageUrl: 'assets/img_friend2.png',
+            name: 'Asep Wibowo',
+            username: 'dajal087176',
+          ),
+          const TransferRecentUserItem(
+            imageUrl: 'assets/img_friend3.png',
+            name: 'Abdul Gofar Maulana',
+            username: 'dajal087176',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildResult() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Result',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const Wrap(
+            spacing: 17,
+            runSpacing: 17,
+            children: [
+              TransferResultUserItem(
+                imageUrl: 'assets/img_friend1.png',
+                isVerified: true,
+                name: 'Dandi Jaelani',
+                username: 'dajal087176',
+              ),
+              TransferResultUserItem(
+                imageUrl: 'assets/img_friend1.png',
+                isVerified: true,
+                name: 'Dandi Jaelani',
+                username: 'dajal087176',
+                isSelected: true,
+              ),
+            ],
           )
         ],
       ),
---
+++ lib/widgets/transfer_recent_user_item.dart
@@ -0,0 +1,92 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class TransferRecentUserItem extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String username;
+  final bool isVerified;
+
+  const TransferRecentUserItem({
+    super.key,
+    required this.imageUrl,
+    this.isVerified = false,
+    required this.name,
+    required this.username,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        bottom: 18,
+      ),
+      padding: const EdgeInsets.all(22),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: whiteColor,
+      ),
+      child: Row(
+        children: [
+          Container(
+            height: 45,
+            width: 45,
+            margin: const EdgeInsets.only(
+              right: 14,
+            ),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  imageUrl,
+                ),
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: blackTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                '@$username',
+                style: greyTextStyle.copyWith(
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          if (isVerified)
+            Row(
+              children: [
+                Icon(
+                  Icons.check_circle,
+                  size: 14,
+                  color: greenColor,
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'Verified',
+                  style: greenTextStyle.copyWith(
+                    fontSize: 11,
+                    fontWeight: medium,
+                  ),
+                ),
+              ],
+            )
+        ],
+      ),
+    );
+  }
+}
