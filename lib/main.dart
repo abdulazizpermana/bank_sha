33bd1c65066c932a09aeac22dbb43f81b0e7f188
abdulazizpermana <abdulazizpermana106@gmail.com>
2024-01-09 22:04:41 +0700

    add auth_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
 import 'package:bank_sha/pages/onboarding_page.dart';
 import 'package:bank_sha/pages/profile/profile-edit-success.dart';
 import 'package:bank_sha/pages/profile/profile_edit.dart';
@@ -20,6 +21,7 @@ import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/pin/pin_page.dart';
 import 'pages/splash_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,44 +31,51 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          scaffoldBackgroundColor: lightBackgorundColor,
-          appBarTheme: AppBarTheme(
-            backgroundColor: lightBackgorundColor,
-            elevation: 0,
-            centerTitle: true,
-            iconTheme: IconThemeData(
-              color: blackColor,
-            ),
-            titleTextStyle: blackTextStyle.copyWith(
-              fontSize: 20,
-              fontWeight: semiBold,
-            ),
-          )),
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/onboarding': (context) => const OnboardingPage(),
-        '/sign-in': (context) => const SignInPage(),
-        '/sign-up': (context) => const SignUpPage(),
-        '/sign-up-setprofile': (context) => const SignUpSetProfile(),
-        '/sign-up-setktp': (context) => const SignUpSetKtp(),
-        '/sign-up-success': (context) => const SignUpSuccess(),
-        '/home': (context) => const HomePage(),
-        '/profile': (context) => const ProfilePage(),
-        '/pin': (context) => const PinPage(),
-        '/profile-edit': (context) => const ProfileEditPage(),
-        '/profile-edit-pin': (context) => const ProfileEditPinPage(),
-        '/profile-edit-success': (context) => const ProfileEditSuccess(),
-        '/topup': (context) => const TopUpPage(),
-        '/topup-amount': (context) => const TopUpAmountPage(),
-        '/topup-success': (context) => const TopUpSuccess(),
-        '/transfer': (context) => const TransferPage(),
-        '/data-provider': (context) => const DataProviderPage(),
-        '/data-package': (context) => const DataPackagePage(),
-        '/data-success': (context) => const DataSuccessPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            scaffoldBackgroundColor: lightBackgorundColor,
+            appBarTheme: AppBarTheme(
+              backgroundColor: lightBackgorundColor,
+              elevation: 0,
+              centerTitle: true,
+              iconTheme: IconThemeData(
+                color: blackColor,
+              ),
+              titleTextStyle: blackTextStyle.copyWith(
+                fontSize: 20,
+                fontWeight: semiBold,
+              ),
+            )),
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/onboarding': (context) => const OnboardingPage(),
+          '/sign-in': (context) => const SignInPage(),
+          '/sign-up': (context) => const SignUpPage(),
+          '/sign-up-setprofile': (context) => const SignUpSetProfile(),
+          '/sign-up-setktp': (context) => const SignUpSetKtp(),
+          '/sign-up-success': (context) => const SignUpSuccess(),
+          '/home': (context) => const HomePage(),
+          '/profile': (context) => const ProfilePage(),
+          '/pin': (context) => const PinPage(),
+          '/profile-edit': (context) => const ProfileEditPage(),
+          '/profile-edit-pin': (context) => const ProfileEditPinPage(),
+          '/profile-edit-success': (context) => const ProfileEditSuccess(),
+          '/topup': (context) => const TopUpPage(),
+          '/topup-amount': (context) => const TopUpAmountPage(),
+          '/topup-success': (context) => const TopUpSuccess(),
+          '/transfer': (context) => const TransferPage(),
+          '/data-provider': (context) => const DataProviderPage(),
+          '/data-package': (context) => const DataPackagePage(),
+          '/data-success': (context) => const DataSuccessPage(),
+        },
+      ),
     );
   }
 }
